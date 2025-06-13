b0f5af049d6d6ecddebc66461b891498584b5c29
Parag <paragsinha55@gmail.com>
2025-06-13 16:52:56 +0530

    added the index route, show route, new route(render a new form for adding new data),

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const  express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
 
+app.set("view engine", "ejs");
+app.set("views" , path.join(__dirname , "views"));
+app.use(express.urlencoded({ extended: true }));
 
 main()                              //connect to database
     .then(()=>{
@@ -16,11 +21,45 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
+//sample listing
+// app.get("/testListing", async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         image: "https://upload.wikimedia.org/wikipedia/commons/2/23/Waves_On_The_Beach_%28184176683%29.jpeg",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     })
 
+//     await sampleListing.save();
+//     console.log("sample was saved");
+
+//     res.send("successful testing");
+// });f
 
 app.get("/", (req,res)=>{
     res.send("it is working");
 });
+
+//INDEX ROUTE 
+app.get("/listings", async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", {allListings});
+});
+
+//create/new route
+app.get("/listings/new", (req,res)=>{
+    res.render("./listings/new.ejs");
+});
+
+//SHOW ROUTE
+app.get("/listings/:id", async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/show.ejs", {listing});
+});
+
 
 app.listen(8080, ()=>{                  //main server
     console.log("Server is listenting on port 8080");
