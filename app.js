651e64d9ffa737605bce9a9b34a709e3be02bf10
Parag <paragsinha55@gmail.com>
2025-06-19 20:04:11 +0530

    added ejs mate

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views" , path.join(__dirname , "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 main()                              //connect to database
     .then(()=>{
@@ -68,6 +73,27 @@ app.post("/listings", async (req,res)=>{
     res.redirect("/listings");
 });
 
+//EDIT ROUTE
+app.get("/listings/:id/edit", async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs", {listing});
+})
+
+//Update route - put
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
 
 app.listen(8080, ()=>{                  //main server
     console.log("Server is listenting on port 8080");
