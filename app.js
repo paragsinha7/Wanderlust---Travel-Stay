51bbfc8f55492135fe519fc84713d15a9f6836b4
Parag <paragsinha55@gmail.com>
2025-07-08 19:08:24 +0530

    updated the app.js file until error handling

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const  express = require("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
+
 
 app.set("view engine", "ejs");
-app.set("views" , path.join(__dirname , "views"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
 main()                              //connect to database
-    .then(()=>{
-    console.log("connected to DB") 
+    .then(() => {
+        console.log("connected to DB")
     })
     .catch(err => {
         console.log(err)
-    });  
+    });
 
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
 //sample listing
@@ -43,58 +47,93 @@ async function main() {
 //     res.send("successful testing");
 // });f
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("it is working");
 });
 
+//Joi middleware
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el=>el.message)).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else{
+        next();
+    }
+}
+
 //INDEX ROUTE 
-app.get("/listings", async (req,res)=>{
+app.get("/listings",  wrapAsync(async (req, res,next) => {
     const allListings = await Listing.find({});
-    res.render("./listings/index.ejs", {allListings});
-});
+    res.render("./listings/index.ejs", { allListings });
+}));
 
 //new route
-app.get("/listings/new", (req,res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("./listings/new.ejs");
 });
 
 //SHOW ROUTE
-app.get("/listings/:id", async (req,res)=>{
-    let {id} = req.params;
+app.get("/listings/:id",  wrapAsync(async (req, res,next) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("./listings/show.ejs", {listing});
-});
+    res.render("./listings/show.ejs", { listing });
+}));
 
 //CREATE ROute
-app.post("/listings", async (req,res)=>{
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
     // let {title, description, image, price, location, country} = req.body;        //to make it shortcut, we give the names as object[keys] in new/ejs
+    // if(!req.body.listing){
+    //     throw new ExpressError(400, "Send valid data");
+    // };
+
+    // let result = listingSchema.validate(req.body);           joi
+    // console.log(result);
+    // if(result.error){
+    //     throw new ExpressError(400, result.error);
+    // }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 //EDIT ROUTE
-app.get("/listings/:id/edit", async (req,res)=>{
-    let {id} = req.params;
+app.get("/listings/:id/edit",  wrapAsync(async (req, res,next) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("./listings/edit.ejs", {listing});
-})
+    res.render("./listings/edit.ejs", { listing });
+}));
 
 //Update route - put
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing,  wrapAsync(async (req, res,next) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    await Listing.findByIdAndUpdate(id, {...req.body.listing });
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //delete route
-app.delete("/listings/:id", async(req,res)=>{
-    let {id} = req.params;
+app.delete("/listings/:id",  wrapAsync(async (req, res,next) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
+}));
+
+//This is the route which gives an error when we are trying
+// to enter a page that hasn't been created
+
+// app.all("*", (req,res,next)=>{
+//     next(new ExpressError(404, "Page not found!"));
+// });
+
+//error handler middleware;
+app.use((err, req, res, next) => {
+    const { statusCode = 404, message = "Something went wrong" } = err;
+    // res.status(statusCode).send(message);
+    res.render("./error.ejs" , {err});
 });
 
-app.listen(8080, ()=>{                  //main server
+app.listen(8080, () => {                  //main server
     console.log("Server is listenting on port 8080");
-})
+}) 
