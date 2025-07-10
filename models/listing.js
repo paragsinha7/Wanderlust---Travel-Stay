0fd407c3233af5f5cf5fe17c968b973f19d9fd21
Parag <paragsinha55@gmail.com>
2025-07-10 14:18:05 +0530

    added the review object in listing model for storing review ids when user adds a review

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({      //defining the schema
     title: {
@@ -9,14 +10,27 @@ const listingSchema = new Schema({      //defining the schema
     description: String,
     image: {
         type: String,
-        default: "https://unsplash.com/photos/coconut-tree-near-body-of-water-HfIex7qwTlI",
-        set: (v) => 
-            v === "" ? "https://unsplash.com/photos/coconut-tree-near-body-of-water-HfIex7qwTlI" 
-            : v,
+        default: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+        set: (v) =>
+            v === "" ? "https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
+                : v,
     },
     price: Number,
     location: String,
-    country: String
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);   //making the model
